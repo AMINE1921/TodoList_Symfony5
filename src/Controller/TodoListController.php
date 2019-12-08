4d431132b63f577f378c2f69b3e1cda46bc22e33
AMINE1921 <amineoubaya1921@gmail.com>
2019-12-09 00:34:39 +0100

    conenxion a la bdd et affichage de la list todo d'un user

--- src/Controller/TodoListController.php
+++ src/Controller/TodoListController.php
@@ -3,17 +3,36 @@
 namespace App\Controller;
 
 
+use App\Entity\ListTodo;
+use App\Repository\ListTodoRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TodoListController extends AbstractController
 {
+
+    private $repository;
+
+    public function __construct(ListTodoRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
     /**
      * @Route("/")
      */
+
     public function index()
     {
+//        $ajout = new ListTodo();
+//        $ajout->setIdUser(1)
+//            ->setTodo('Deuxieme test');
+//        $em = $this->getDoctrine()->getManager();
+//        $em->persist($ajout);
+//        $em->flush();
 
-        return $this->render('home/home.html.twig');
+    $todo = $this->repository->findAll();
+        return $this->render('home/home.html.twig', [
+        'list' => $todo]);
     }
 }
