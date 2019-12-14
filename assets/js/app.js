0d90186be02fa3065b6bd30717e84b16f7f66169
AMINE1921 <amineoubaya1921@gmail.com>
2019-12-14 13:15:15 +0100

    Ajout de webpack et correction des bugs

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,6 +9,47 @@
 require('../css/app.css');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-// const $ = require('jquery');
+const $ = require('jquery');
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+$(function () {
+    var $list, $li, $newItemForm, $div;
+    $list = $('ul.items');
+    $newItemForm = $('#newItemForm');
+    $li = $('ul#items li');
+
+    function cacher() {
+        $('.close').click(function () {
+            $(this).parent().remove();
+        })
+    };
+
+    $(".msg").click(function () {
+        location.reload(true);
+    });
+
+    cacher();
+
+    $newItemForm.on('submit', function (e) {
+        e.preventDefault();
+        var text = $('input:text').val();
+        var spann = $('<span class="close">\u00D7</span>');
+        if (text !== '') {
+            $list.append('<li>' + text + ' <span class="close">\u00D7</span> </li>');
+            $('input:text').val('');
+            cacher();
+        }
+    });
+
+    $list.on('click', 'li', function () {
+        $(this).toggleClass("checked");
+    });
+
+    function functionInterdit() {
+        $('#newItemForm2').click(function () {
+            alert("Vous devez vous connecter !");
+        })
+    };
+
+    functionInterdit();
+
+})
