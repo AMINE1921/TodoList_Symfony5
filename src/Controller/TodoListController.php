af5e589a54c36c44fa8850b912d01a5de482df80
AMINE1921 <amineoubaya1921@gmail.com>
2020-01-21 10:33:08 +0100

    Ajout de la suppression et version final du site

--- src/Controller/TodoListController.php
+++ src/Controller/TodoListController.php
@@ -4,14 +4,19 @@ namespace App\Controller;
 
 
 use App\Entity\ListTodo;
+use App\Form\TodoType;
 use App\Repository\ListTodoRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
 class TodoListController extends AbstractController
 {
-
+    /**
+     * @var ListTodoRepository
+     */
     private $repository;
 
     public function __construct(ListTodoRepository $repository)
@@ -20,13 +25,42 @@ class TodoListController extends AbstractController
     }
 
     /**
-     * @Route("/", name="home")
+     * @Route("/", name="home", methods="GET|POST")
      */
 
-    public function index()
+    public function new(Request $request): Response
     {
+        $task = new ListTodo();
+        $users = $this->getUser();
+        if (TRUE === $this->get('security.authorization_checker')->isGranted('ROLE_USER')) {
+            $task->setIdUser($users->getId());
+        }
+        $form = $this->createForm(TodoType::class, $task);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($task);
+            $entityManager->flush();
+        }
         $todo = $this->repository->findAll();
+
         return $this->render('home/home.html.twig', [
-            'list' => $todo]);
+            'form' => $form->createView(), 'list' => $todo
+        ]);
+    }
+
+    /**
+     * @Route("/delete/{id}", name="delete", methods="DELETE")
+     */
+    public function delete(ListTodo $tasks, Request $request)
+    {
+        if ($this->isCsrfTokenValid('delete' . $tasks->getId(), $request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($tasks);
+            $entityManager->flush();
+            return $this->redirectToRoute('home');
+        }
+        return $this->redirectToRoute('home');
     }
 }
